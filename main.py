8d5862b0abd974467e7fde2acab6732a14645028
lohputer <leynalyndon@gmail.com>
2025-01-15 23:17:01 +0800

    sec 4 instalment!

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import random
-def format_sentences(sentences):
+def format_chengyu(sentences):
     print("running")
     sentences = sentences.split("\n\n")
     ciyus = []
@@ -20,133 +20,8 @@ def format_sentences(sentences):
     with open("hypy.txt", "a", encoding='utf-8') as f1:
         for line in hypys:
             f1.write(line + "\n")
-    
-text = """
-221. 一暴十寒（yī pù shí hán）
-解释：比喻学 习工作没有恒心，努力的时候少，懒惰的时候多。
-例：成功靠的是毅力和恒心，像你这样一暴 十寒，怎么会有成就呢？ 
 
-222．一视同仁（yī shì tóng rén）
-解释：一样看待，仁爱。指平等对待，不加区别。
-例：这个公司的老板对员工一视同仁，勤奋的职员都有机会获得升职。
-
-223．一意孤行（yī yì gū xíng）
-解释：固执地按照自己的心意做事，不听别人的意见，独断独行。
-例：你应该认真考虑大家的意见，不应再这样一意孤行，蛮干下去。
-
-224．一针见血（yī zhēn jiàn xuè）
-解释：比喻说话简要，一下子就说中要害。
-例：他的批评一针见血，切中小王的要害，令假公济私的小王很不安。
-
-225．以卵击石（yǐ luǎn jī shí）
-解释：拿蛋去打石头。比喻以弱敌强，自取灭亡。
-例：他是我国羽毛球单打冠军，林教练派你去和他对阵，岂不是以卵击石？
-
-226．以身作则（yǐ shēn zuò zé）
-解释：用自己的实际行动做榜样。
-例：他不但教育子女要养成看书的好习惯，而且以身作则，每天陪子女阅读书报。
-
-227．以牙还牙（yǐ yá huán yá）
-解释：别人用牙齿咬我，我也用牙齿咬他。比喻用对方使用的手段回击对方。
-例：每次我做错事，他就得理不饶人；这次他做错了事，我决定以牙还牙，让他尝尝被欺负的滋味。
-
-228．因材施教（yīn cái shī jiào）
-解释：根据不同的对象或智力的高低而采取不同的教育方法。
-例：校长要求每位老师能因材施教，使不同程度的学生都能发展他们最大的潜能。
-
-229．饮水思源（yǐn shuǐ sī yuán）
-解释：喝水时想到水源。比喻不忘本。
-例：你能有今天的成就，应该饮水思源，感谢王老师的指导与帮助。
-
-230．迎刃而解（yíng rèn ér jiě）
-解释：比喻主要问题解决了，其他问题就容易办。也比喻问题顺利解决。
-例：这个关键的难题一解决，其他问题也就迎刃而解了。
-
-231．应接不暇（yìng jiē bù xiá）
-解释：形容事情繁多，忙得不可开交，无法应付。
-例：学校假期中，每天前来借书的学生很多，图书馆的工作人员忙得不可开交，应付不过来。
-
-232．与虎谋皮（yǔ hǔ móu pí）
-解释：比喻商量的事危害对方切身利益，绝对办不成。
-例：这间店的生意越来越好，你想要店主转让，这简直是与虎谋皮！
-
-233．与日俱增（yǔ rì jù zēng）
-解释：形容随着时间不断增长。
-例：随着服务质量的改善，那间餐馆的生意蒸蒸日上，营业额与日俱增。
-
-234．缘木求鱼（yuán mù qiú yú）
-解释：比喻方向或方法不对，不可能达到目的。
-例：这孩子根本没有音乐天分，家长硬要他学音乐，简直是缘木求鱼，白费心机。
-
-235．约法三章（yuē fǎ sān zhāng）
-解释：指订立为大家所遵守的规章条款。
-例：考试期间爸爸和我约法三章，不许我看电视连续剧。
-
-236．再接再厉（zài jiē zài lì）
-解释：比喻继续努力，毫不放松。
-例：在进入半决赛后，陈教练鼓励全体球员再接再厉，争取进入大决赛。
-
-237．真知灼见（zhēn zhī zhuó jiàn）
-解释：正确而透彻的见解。
-例：黄教授所发表的言论确实是真知灼见，深受学术界的赞誉。
-
-238．蒸蒸日上（zhēng zhēng rì shàng）
-解释：形容事业一天天向上发展。
-例：我们伟大的祖国生机勃勃，蒸蒸日上。
-
-239．执迷不悟（zhí mí bù wù）
-解释：坚持错误而不觉悟。
-例：尽管老师苦口婆心地劝他改过自新，但他仍执迷不悟。
-
-240．纸上谈兵（zhǐ shàng tán bīng）
-解释：比喻空谈理论，不能解决实际问题。
-例：理论不能联系实际，只会纸上谈兵，是不可能成功的。
-
-241．志同道合（zhì tóng dào hé）
-解释：志向相同，意见一致。形容有共同的理想和目标。
-例：我和小华自小相识，兴趣爱好一致，是志同道合的好朋友。
-
-242．志在四方（zhì zài sì fāng）
-解释：形容有远大的志向和抱负。
-例：男儿志在四方，将来有机会我要到国外去发展事业。
-
-243．州官放火（zhōu guān fàng huǒ）
-解释：比喻在上的人可以胡作非为，而普通人却受到限制。
-例：经理不许职员把任何过期的旧报纸带回家，自己却每天把当天的报纸带走，真是只许州官放火，不许百姓点灯。
-
-244．装聋作哑（zhuāng lóng zuò yǎ）
-解释：指故意不理睬，只当不知道。
-例：对社会上的不良风气，不能装聋作哑，不闻不问。
-
-245．自暴自弃（zì bào zì qì）
-解释：自己瞧不起自己，甘于落后或堕落。
-例：虽然这次比赛失败了，但我们不能自暴自弃，要继续努力，争取下次赢回来。
-
-246．自食其力（zì shí qí lì）
-解释：依靠自己的劳动所得来生活。
-例：年轻人要自食其力，总想着依靠父母是不会有出息的。
-
-247．自投罗网（zì tóu luó wǎng）
-解释：自己主动钻入对方布下的陷阱，也指自取祸害。
-例：这名毒犯有眼无珠，竟然向警方兜售毒品，真是自投罗网。
-
-248．自相矛盾（zì xiāng máo dùn）
-解释：比喻语言、行动前后不一致或互相抵触。
-例：将军原本主张抵抗到底，现在却又主张同敌人讲和，真是自相矛盾。
-
-249．作壁上观（zuò bì shàng guān）
-解释：比喻站在一旁看着，不动手帮助。
-例：他看到别人有困难，总是作壁上观，从来也不会伸出援手。
-
-250．坐享其成（zuò xiǎng qí chéng）
-解释：自己不出力而享受别人取得的成果。
-例：李经理从没出过力，计划完成后他却坐享其成，将功劳归于自己，大家都感到愤愤不平。
-"""
-
-with open("ciyu.txt", encoding='utf-8') as f : ciyus = f.read().split('\n')
-with open("hypy.txt", encoding='utf-8') as f : hypys = f.read().split('\n')
-with open("zaoju.txt", encoding='utf-8') as f : zaojus = f.read().split('\n')
-with open("meanings.txt", encoding='utf-8') as f : meanings = f.read().split('\n')
+ciyus, hypys, zaojus, meanings = [], [], [], []
 chosenNums = []
 wrongWords = {}
 correctWords = []
@@ -160,7 +35,14 @@ while not exited:
     zaojuWordLst = []
     zaojusLst = []
     chosenNums = []
-    print("Welcome to the Sec 3 Chinese Revision chatbot :O")
+    print("Welcome to the Chinese Revision chatbot :O")
+    level = input("Pick a level of Chinese (Sec 1 to 4): ").strip()
+    while level not in ["1", "2", "3", "4"]:
+        level = input("Invalid! Try again. Pick a level of Chinese (Sec 1 to 4): ")
+    with open(f"sec{level}/ciyu.txt", encoding='utf-8') as f : ciyus = f.read().split('\n')
+    with open(f"sec{level}/hypy.txt", encoding='utf-8') as f : hypys = f.read().split('\n')
+    with open(f"sec{level}/zaoju.txt", encoding='utf-8') as f : zaojus = f.read().split('\n')
+    with open(f"sec{level}/meanings.txt", encoding='utf-8') as f : meanings = f.read().split('\n')
     print("""
 This chatbot has various functions:
 1) Generate 选词填空 
@@ -174,28 +56,37 @@ Desc: Set the words you want to test and a random ciyu will be given. Spend some
 
 4) Word List
 Desc: After selecting this function, you can ask for any series of words and the list of ciyus will be printed.
+          
+5) 汉语拼音
+Desc: Given a 汉语拼音, write out the word yourself (on a piece of paper), and check after
 """)
-    func = input("What function do you want to pick? (1/2/3/4): ")
-    while len(func) != 1 or func not in "1234":
+    func = input("What function do you want to pick? (1/2/3/4/5): ")
+    while len(func) != 1 or func not in "12345":
         print("Invalid. Try again!")
-        func = input("What function do you want to pick? (1/2/3/4): ")
+        func = input("What function do you want to pick? (1/2/3/4/5): ")
 
-    if func == "1" or func == "3" or func == "4":
-        limits = input("""
-In one string together, with spaces, set the lower bound and upper bound
-A) 单元一----林黛玉进贾府 (0-11)
-B) 单元二----吃茶喝茶品茶 (12-25)
-C) 单元二----喜怒衰乐与健康 (26-36)
-D) 单元三----远亲不如近邻 (37-48)
-E) 单元三----老吾老以及人之老 (49-59)
-F) 成语 part 1 (60-119)
-G) 单元四----沟通面面观 (120-131)
-H) 单元四----出言有尺 交往有度 (132-143)
-I) 单元五----学然后知不足 (144-156)
-J) 单元五----学然后知不足 (157-167)
-K) 成语 part 2 (168-197)
-                    
-""")
+    limitText = {
+        "1": """""",
+        "2": """""",
+        "3": """
+A) Sec 3 单元一----林黛玉进贾府 (0-11)
+B) Sec 3 单元二----吃茶喝茶品茶 (12-25)
+C) Sec 3 单元二----喜怒衰乐与健康 (26-36)
+D) Sec 3 单元三----远亲不如近邻 (37-48)
+E) Sec 3 单元三----老吾老以及人之老 (49-59)
+F) Sec 3 成语 part 1 (60-119)
+G) Sec 3 单元四----沟通面面观 (120-131)
+H) Sec 3 单元四----出言有尺 交往有度 (132-143)
+I) Sec 3 单元五----学然后知不足 (144-156)
+J) Sec 3 单元五----学然后知不足 (157-167)
+K) Sec 3 成语 part 2 (168-197)               
+""",
+        "4": """
+A) Sec 4 单元二---敬业乐业（0-9）
+"""
+    }
+    if func == "1" or func == "3" or func == "4" or func == "5":
+        limits = input("In one string together, with spaces, set the lower bound and upper bound\n" + limitText[level])
         lowBound = int(limits.split(" ")[0])
         highBound = int(limits.split(" ")[1])
         if func == "1" or func == "3":
@@ -272,12 +163,17 @@ K) 成语 part 2 (168-197)
                     print(f"The actual 意思：{meanings[num]}")
                     print(f"Proper example of a 造句：{zaojus[num]}")
                     print()
-        else: 
+        elif func == "4": 
             for i in range(lowBound, highBound):
                 print(f"词语：{ciyus[i]} {hypys[i]}")
                 print(f"意思：{meanings[i]}")
                 print(f"造句：{zaojus[i]}")
                 input("")
+        elif func == "5":
+            chosenNums = []
+            for i in range(lowBound, highBound):
+                chosenNums.append(random.randint(lowBound, highBound-1))
+                print(f"{hypys[chosenNums[-1]]}")
     else:
         exit2 = False
         while not exit2:
